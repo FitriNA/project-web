1cb92b624c89f241200085e805be625afd298112
user\ADMIN <faini214@gmail.com>
2016-12-21 11:51:51 +0700

    test

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<title>perpuz</title>
+  <link href="admin/lib/css/gaya.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="admin/lib/css/bootstrap.css">
+	<link rel="stylesheet" href="admin/lib/css/bootstrap-theme.min.css">
+</head>
+<body style="background-image: url(admin/lib/css/images/bg.png); background-size: cover;">
+<nav class="navbar navbar-default">
+  <div class="container">
+    <a class="navbar-brand"><b><i>perpuz</i></b></a>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">Beranda</a></li>
+        <li><a href="buku.php">Buku</a></li>
+        <li><a href="anggota.php">Anggota</a></li>
+      </ul>
+  </div>
+</nav>
+<div class="container">
+
+</div>
+</body>
+  <script src="admin/lib/js/jquery.min.js"></script>
+  <script src="admin/lib/js/bootstrap.min.js"></script>
+</html>
