9531807f7badc01d601a10b9a2fbe733bd2e451d
hellraizer\huda <m45.hud4@gmail.com>
2017-01-04 11:49:12 +0700

    penambahan fungsi pencarian pada laman anggota..

--- anggota.php
+++ anggota.php
@@ -19,6 +19,12 @@
   </div>
 </nav>
 <div class="container">
+	<form method="post" action="" class="form-inline">
+		<div class="form-group">
+			<input type="text" name="search" placeholder="Pencarian" class="form-control"/>
+			<input type="submit" name="cari" value="cari" class="btn btn-success"><p></p>
+		</div>
+	</form>
 	<table class="table table-bordered">
 		<tr>
 			<th>No</th>
@@ -29,10 +35,38 @@
 			<th>Alamat</th>
 			<th>E-Mail</th>
 			<th>Tgl Masuk</th>
+		</tr>
 			<?php
 				include('admin/fungsi/koneksi.php');
 				$query=mysql_query("SELECT * FROM anggota ORDER BY id_anggota DESC") or die(mysql_error());
-				if(mysql_num_rows($query) == 0){
+				if (isset($_POST['cari'])) {
+					$search = $_POST['search'];
+
+					$sql = "SELECT * FROM anggota WHERE id_anggota LIKE '%$search%' OR nama LIKE '%$search%' OR kelamin LIKE '%$search%' OR phone LIKE '%$search%' OR alamat LIKE '%$search%' OR email LIKE '%$search%' OR tgl_entry LIKE '%$search%'";
+					$result = mysql_query($sql) or die('Ada kesalahan saat menampilkan data.' . mysql_error());
+					
+					if (mysql_num_rows($result) == 0) {
+						echo '<p></p><p>Pencarian tidak ditemukan</p>';
+					} else {
+						echo '<p></p>';
+						$no = 1;
+						while($row=mysql_fetch_array($result)){
+						extract($row);
+							echo '<tr class=hijau>';
+							echo '<td>'.$no.'</td>';
+							echo '<td>'.$id_anggota.'</td>';
+							echo '<td>'.$nama.'</td>';
+							echo '<td>'.$kelamin.'</td>';
+							echo '<td>'.$phone.'</td>';
+							echo '<td>'.$alamat.'</td>';
+							echo '<td>'.$email.'</td>';
+							echo '<td>'.$tgl_entry.'</td>';
+							echo '</tr>';
+						$no++;
+						}
+					}
+				}
+				else if(mysql_num_rows($query) == 0){
 					echo '<tr><td colspan="8">Tidak ada data!</td></tr>';
 				}else{
 					$no=1;
